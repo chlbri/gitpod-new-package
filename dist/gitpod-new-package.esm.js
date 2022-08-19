1e39a21c2c22368fa016809af52f0ebfb222af23
chlbri <bri_lvi@icloud.com>
2022-08-19 16:56:44 +0000

    ci: pack

--- dist/gitpod-new-package.esm.js
+++
@@ -1,6 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-
-export { sum };
-//# sourceMappingURL=gitpod-new-package.esm.js.map
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./gitpod-new-package.cjs.production.min.js')
-} else {
-  module.exports = require('./gitpod-new-package.cjs.development.js')
-}
