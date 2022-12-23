81eb9831a1f0b452ddfe505444d66e2538360f10
chlbri <bri_lvi@icloud.com>
2022-12-23 15:31:16 +0000

    new version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,38 +1,33 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import dts from 'rollup-plugin-dts';
-import esbuild from 'rollup-plugin-esbuild';
-import { terser } from 'rollup-plugin-terser';
 import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
-
-/** @type {import('rollup').defineConfig} */
-const bundle = config => ({
-  ...config,
-  input: 'src/index.ts',
-  external: id => !/^[./]/.test(id),
-});
+import typescript from 'rollup-plugin-typescript2';
 
 /** @type {import('rollup').RollupOptions} */
-export default [
-  bundle({
-    plugins: [esbuild(), terser({})],
-    output: [
-      {
-        file: `lib/index.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: `lib/index.mjs`,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-  }),
-  bundle({
-    plugins: [dts()],
-    output: {
-      file: `lib/index.d.ts`,
+export default {
+  input: 'src/index.ts',
+  plugins: [typescript(), tsConfigPaths()],
+  external: [
+    /* TODO: Add external libraries */
+  ],
+  output: [
+    {
       format: 'es',
+      file: 'lib/index.d.ts',
     },
-  }),
-];
+    {
+      format: 'cjs',
+      sourcemap: true,
+      dir: `lib`,
+      preserveModulesRoot: 'src',
+      preserveModules: true,
+      entryFileNames: '[name].js',
+    },
+    {
+      format: 'es',
+      sourcemap: true,
+      dir: `lib`,
+      preserveModulesRoot: 'src',
+      preserveModules: true,
+      entryFileNames: '[name].mjs',
+    },
+  ],
+};
